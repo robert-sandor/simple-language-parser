c562a58dea7ae2bed119c36296855a498186f750
Robert Sandor <sandorrobertk94@gmail.com>
2017-01-15 02:36:56 +0200

    implemented follow method

--- src/ro/sandorr/Main.java
+++ src/ro/sandorr/Main.java
@@ -6,6 +6,8 @@ import ro.sandorr.parser.LLOneParser;
 
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Map;
+import java.util.Set;
 
 public class Main {
 
@@ -13,7 +15,9 @@ public class Main {
         Path grammarFile = Paths.get("grammar.txt");
         try {
             Grammar grammar = GrammarBuilder.fromFile(grammarFile);
-            System.out.println(LLOneParser.first(grammar));
+            Map<String, Set<String>> first = LLOneParser.first(grammar);
+            System.out.println(first);
+            System.out.println(LLOneParser.follow(grammar, first));
         } catch (RuntimeException e) {
             e.printStackTrace();
         }
--- src/ro/sandorr/parser/LLOneParser.java
+++ src/ro/sandorr/parser/LLOneParser.java
@@ -1,14 +1,19 @@
 package ro.sandorr.parser;
 
 import ro.sandorr.grammar.Grammar;
+import ro.sandorr.grammar.Production;
 
 import java.util.*;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
  * Created by sando on 1/7/2017.
  */
 public class LLOneParser {
+
+    public static final String DOLLAR = "$";
+
     public static Map<String, Set<String>> first(final Grammar grammar) {
         final Map<String, Set<String>> map = new HashMap<>();
 
@@ -60,9 +65,59 @@ public class LLOneParser {
                     }));
         }
 
-        grammar.getTerminals().forEach(map::remove);
-        map.remove(Grammar.EPSILON);
+//        grammar.getTerminals().forEach(map::remove);
+//        map.remove(Grammar.EPSILON);
 
         return map;
+    }
+
+    public static Map<String, Set<String>> follow(final Grammar grammar, final Map<String, Set<String>> firstResult) {
+        final Map<String, Set<String>> followResult = new HashMap<>();
+
+        grammar.getNonTerminals().forEach(nonTerminal -> followResult.put(nonTerminal, new HashSet<>()));
+        followResult.get(grammar.getStart()).add(Grammar.EPSILON);
+
+        final boolean[] changed = {true};
+        while (changed[0]) {
+            changed[0] = false;
+            grammar.getNonTerminals().forEach(nonTerminal -> grammar.getProductions().stream()
+                    .filter(production -> production.getRightHand().contains(nonTerminal))
+                    .forEach(production -> {
+                        final List<String> rhs = production.getRightHand();
+                        final List<String> sequence = rhs.subList(rhs.indexOf(nonTerminal) + 1, rhs.size());
+
+
+                        final Set<String> firstOfSequence = firstSequence(firstResult, sequence);
+                        final Set<String> firstOfSequenceWithoutEpsilon = firstOfSequence.stream()
+                                .filter(token -> !token.equals(Grammar.EPSILON)).collect(Collectors.toSet());
+
+                        if (!followResult.get(nonTerminal).containsAll(firstOfSequenceWithoutEpsilon)) {
+                            followResult.get(nonTerminal).addAll(firstOfSequenceWithoutEpsilon);
+                            changed[0] = true;
+                        }
+
+                        if (firstOfSequence.contains(Grammar.EPSILON)) {
+                            if (!followResult.get(nonTerminal).containsAll(followResult.get(production.getLeftHand().get(0)))) {
+                                followResult.get(nonTerminal).addAll(followResult.get(production.getLeftHand().get(0)));
+                                changed[0] = true;
+                            }
+                        }
+                    }));
+        }
+
+        return followResult;
+    }
+
+    public static Set<String> firstSequence(final Map<String, Set<String>> first, final List<String> sequence) {
+        Set<String> result = new HashSet<>();
+        result.add(Grammar.EPSILON);
+        for (String token : sequence) {
+            result.addAll(first.get(token));
+            if (!first.get(token).contains(Grammar.EPSILON)) {
+                result.remove(Grammar.EPSILON);
+                break;
+            }
+        }
+        return result;
     }
 }
